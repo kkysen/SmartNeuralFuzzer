8a4181ac4689834056f3fe12e454cf21217c11c7
Khyber <kkysen@gmail.com>
2019-06-05 15:30:13 -0400

    Added O_CLOEXEC to runtime output file descriptors.

--- src/main/runtime/coverage/CoverageOutput.cpp
+++ src/main/runtime/coverage/CoverageOutput.cpp
@@ -11,7 +11,10 @@ namespace runtime::coverage {
     }
     
     io::Writer writer(const fse::Dir& dir, const std::string& name) {
-        return io::Writer(dir.file(name + ".bin", O_WRONLY | O_CREAT | O_TRUNC, 0644));
+        // O_CLOEXEC is so the files all close on exec(), after which our runtime will be gone
+        // the exec hooks should handle all this anyway,
+        // but it's safer to have the kernel handle it also
+        return io::Writer(dir.file(name + ".bin", O_WRONLY | O_CREAT | O_TRUNC | O_CLOEXEC, 0644));
     }
     
 }
