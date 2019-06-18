dd7f4cfb765c743b1b4e513bd996aaa80be5dd91
Khyber <kkysen@gmail.com>
2019-06-18 17:17:22 -0400

    Tested the LEB128 encoding and it appears to work. For `./nm-new.coverage $(which opt-9-debug) | ./cxxfilt.coverage > /dev/null`, blocks.bin got about 7x smaller, but nonSingle.bin only got about 2x smaller.

--- src/share/io/LEB128WriteBuffer.h
+++ src/share/io/LEB128WriteBuffer.h
@@ -17,6 +17,15 @@ namespace io {
         u8 on(i64 value, u8 padTo = 0) noexcept;
         
         template <typename T>
+        u8 on(T t) noexcept {
+            if constexpr (std::is_signed_v<T>) {
+                return on(static_cast<i64>(t));
+            } else {
+                return on(static_cast<u64>(t));
+            }
+        }
+        
+        template <typename T>
         LEB128WriteBuffer& operator<<(T t) noexcept {
             on(t);
             return *this;
